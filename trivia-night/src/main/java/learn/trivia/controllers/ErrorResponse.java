ae1d0a500694f465d4e795fdd34b66e29ee1e720
Tyler <tyluitjens@gmail.com>
2021-02-16 11:31:41 -0600

    Adds Error Response

--- trivia-night/src/main/java/learn/trivia/controllers/ErrorResponse.java
+++ trivia-night/src/main/java/learn/trivia/controllers/ErrorResponse.java
@@ -1,4 +1,22 @@
-package main.java.learn.trivia.controllers;
+package learn.trivia.controllers;
 
+import learn.trivia.domain.Result;
+import learn.trivia.domain.ResultType;
+import learn.trivia.models.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = {"http://localhost:3000"})
 public class ErrorResponse {
+
+    public static <T> ResponseEntity<Object> build(Result<T> result) {
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        if (result.getType() == null || result.getType() == ResultType.INVALID) {
+            status = HttpStatus.BAD_REQUEST;
+        } else if (result.getType() == ResultType.NOT_FOUND) {
+            status = HttpStatus.NOT_FOUND;
+        }
+        return new ResponseEntity<User>(result.getMessages(), status);
+    }
 }
