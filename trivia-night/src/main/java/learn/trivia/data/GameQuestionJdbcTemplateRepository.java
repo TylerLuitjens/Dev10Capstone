95f7d3351f2daeb42319a10162730e10adf5fc5c
Tyler <tyluitjens@gmail.com>
2021-02-18 10:11:35 -0600

    Adds functionality to GameQuestion repo and fixes inner join for mapper

--- trivia-night/src/main/java/learn/trivia/data/GameQuestionJdbcTemplateRepository.java
+++ trivia-night/src/main/java/learn/trivia/data/GameQuestionJdbcTemplateRepository.java
@@ -1,9 +1,12 @@
 package learn.trivia.data;
 
+import learn.trivia.data.mappers.GameQuestionMapper;
+import learn.trivia.data.mappers.GameUserMapper;
 import learn.trivia.models.GameQuestion;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.PreparedStatement;
 import java.util.List;
 
 @Repository
@@ -16,27 +19,49 @@ public class GameQuestionJdbcTemplateRepository implements GameQuestionRepositor
 
     @Override
     public GameQuestion getGameQuestion(String gameCode, int questionId) {
-        String sql = "SELECT * FROM game_question WHERE game_code = ? AND question_id = ?;";
-        return null;
+        String sql = "SELECT * FROM game_question " +
+                "INNER JOIN question ON game_question.question_id = question.question_id " +
+                "WHERE game_code = ?;";
+
+        return template.query(sql, new GameQuestionMapper(), gameCode, questionId).stream().findFirst().orElse(null);
     }
 
     @Override
     public List<GameQuestion> getGameQuestionsByGame(String gameCode) {
-        String sql = "SELECT * FROM game_question WHERE game_code = ?;";
-        return null;
+        String sql = "SELECT * FROM game_question " +
+                "INNER JOIN question ON game_question.question_id = question.question_id " +
+                "WHERE game_code = ?;";
+
+        return template.query(sql, new GameQuestionMapper(), gameCode);
     }
 
     @Override
     public boolean addGameQuestion(String gameCode, int questionId) {
         String sql = "INSERT INTO game_question (game_code, question_id)" +
                 "VALUES (?,?);";
-        return false;
+
+        int rowsAffected = template.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, gameCode);
+            ps.setInt(2, questionId);
+            return ps;
+        });
+
+        return rowsAffected > 0;
     }
 
     @Override
     public boolean addGameQuestion(GameQuestion gameQuestion) {
         String sql = "INSERT INTO game_question (game_code, question_id)" +
                 "VALUES (?,?);";
-        return false;
+
+        int rowsAffected = template.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, gameQuestion.getGameCode());
+            ps.setInt(2, gameQuestion.getQuestionId());
+            return ps;
+        });
+
+        return rowsAffected > 0;
     }
 }
--- trivia-night/src/main/java/learn/trivia/data/mappers/GameQuestionMapper.java
+++ trivia-night/src/main/java/learn/trivia/data/mappers/GameQuestionMapper.java
@@ -10,12 +10,12 @@ import java.sql.SQLException;
 public class GameQuestionMapper implements RowMapper<GameQuestion> {
     @Override
     public GameQuestion mapRow(ResultSet resultSet, int i) throws SQLException {
+
         GameQuestion gameQuestion = new GameQuestion();
         gameQuestion.setQuestionId(resultSet.getInt("question_id"));
         gameQuestion.setGameCode(resultSet.getString("game_code"));
-        // FIXME this doesn't have a question column currently, so we need to get the question from the question repo
-        // Would it be bad to do this in the controller?
-//        gameQuestion.setQuestion(resultSet.getString("question"));
+        gameQuestion.setQuestion(resultSet.getString("question"));
+
         return gameQuestion;
     }
 }
