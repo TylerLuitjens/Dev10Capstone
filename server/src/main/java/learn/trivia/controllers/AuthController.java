293a4bcc6a5c71fc46b39915a80a93646fdeb125
Max <mbmorris@gmail.com>
2021-02-23 19:39:44 -0600

    delete bearer tokens

--- server/src/main/java/learn/trivia/controllers/AuthController.java
+++ server/src/main/java/learn/trivia/controllers/AuthController.java
@@ -12,13 +12,11 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@EnableWebMvc
 public class AuthController {
     private final AuthenticationManager authenticationManager;
     private final JwtConverter jwtConverter;
